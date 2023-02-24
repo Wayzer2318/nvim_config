a5c80d5a791e785b6bf6e3033a7fa6461e2c6ca2
Wayzer2318 <arezki.ait.kettout@gmail.com>
2023-02-24 13:57:27 +0100

    adding nvim config

---
+++ init.lua
@@ -0,0 +1,23 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+
+require("lazy").setup({
+  {
+    "nvim-neorg/neorg",
+    build = ":Neorg sync-parsers",
+    opts = {
+      load = {
+        ["core.defaults"] = {}, -- Loads default behaviour
+        ["core.norg.concealer"] = {}, -- Adds pretty icons to your documents
+        ["core.norg.dirman"] = { -- Manages Neorg workspaces
+          config = {
+            workspaces = {
+              notes = "~/notes",
+            },
+          },
+        },
+      },
+    },
+    dependencies = { { "nvim-lua/plenary.nvim" } },
+  },
+})
